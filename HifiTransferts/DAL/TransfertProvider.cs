838301dd3f0e289af5db5c070c2c460e15a39e29
chiwawaweb <git@chiwawaweb.com>
2017-12-23 14:41:41 +0100

    Maj

--- HifiTransferts/DAL/TransfertProvider.cs
+++ HifiTransferts/DAL/TransfertProvider.cs
@@ -23,5 +23,33 @@ namespace HifiTransferts.DAL
                 }
             }
         }
+
+        public List<Transfert> Search(string keywords)
+        {
+            using (Context context = new Context())
+            {
+                try
+                {
+                    var transferts = from b in context.Transferts
+                                 orderby b.Id descending
+                                 /*
+                                 where (((b.InfoClient1.Contains(keywords)
+                                    || (b.InfoClient2.Contains(keywords))
+                                    || (b.InfoClient3.Contains(keywords))
+                                    || (b.InfoClient4.Contains(keywords)))
+                                 && (b.Date >= dateDebut)
+                                 && (b.Date <= dateFin))
+                                 || b.Numero.Equals(keywords))
+                                 */
+                                 select b;
+                    return transferts.ToList();
+                }
+                catch
+                {
+                    throw;
+                }
+            }
+
+        }
     }
 }
--- HifiTransferts/Forms/TransfertsListForm.cs
+++ HifiTransferts/Forms/TransfertsListForm.cs
@@ -16,6 +16,8 @@ namespace HifiTransferts.Forms
     {
         private static int idRetour;
 
+        TransfertProvider transfertProvider = new TransfertProvider();
+
         public TransfertsListForm()
         {
             InitializeComponent();
@@ -30,14 +32,38 @@ namespace HifiTransferts.Forms
         private void CreateTable(List<Transfert> list, int _idRetour)
         {
             idRetour = _idRetour;
+            /* Initialisation du Datagridview */
+            dgvTransferts.Rows.Clear();
+            dgvTransferts.Columns.Clear();
+
+            /* Mise en forme Datagridview */
+            DataGridViewTextBoxColumn idColumn = new DataGridViewTextBoxColumn();
+            idColumn.Name = "ID";
+            idColumn.HeaderText = "#";
+            idColumn.Visible = true;
+
+            DataGridViewTextBoxColumn dateColumn = new DataGridViewTextBoxColumn();
+            dateColumn.Name = "Date";
+            dateColumn.HeaderText = "DATE";
+            dateColumn.Width = 90;
+            dateColumn.MinimumWidth = 90;
+            dateColumn.FillWeight = 1;
+            dateColumn.HeaderCell.Style.Alignment = DataGridViewContentAlignment.MiddleCenter;
+            dateColumn.DefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleCenter;
+
+
+            /* Création des colonnes */
+            dgvTransferts.Columns.Add(idColumn);
+            dgvTransferts.Columns.Add(dateColumn);
+            
         }
 
         private void RefreshData()
         {
             List<Transfert> list;
-            /*
+            list = transfertProvider.Search(""); // à completer avec mots cles / dates
+
             CreateTable(list, idRetour);
-            */
         }
     }
 }
